459efa8da51ffe07ffb2dc8d3c64c85759f0b879
Aye Myat Mon <mon@Ayes-MacBook-Pro.local>
2023-03-25 23:39:53 +0800

    setup gh-pages

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -4,7 +4,7 @@ import { FiSearch } from "react-icons/fi";
 
 const Recipe = ({ item }) => {
   return (
-    <div className="my-custom-item-wrap w-[50%] md:w-[33.33%] lg:w-[33.33%] box-border px-[5px] md:px-[10px] mb-7 text-center">
+    <div className="my-custom-item-wrap w-[50%] md:w-[33.33%] lg:w-[33.33%] box-border px-[5px] md:px-[10px] mb-[10px] md:mb-[20px] text-center">
       <Link to={`/detail/${item.idMeal}`}>
         <img src={item.strMealThumb} className="rounded" />
         {/* <h3 className='p-2 text-lg'>{item.strMeal}</h3> */}
