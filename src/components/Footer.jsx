47f2b3765e8471a4e4258005010e0c595edacbf1
Aye Myat Mon <mon@Ayes-MacBook-Pro.local>
2023-03-25 23:36:03 +0800

    setup gh-pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="px-2 py-6 mt-10 md:mt-20 bg-black text-white text-center">
-      Thank you. Presented by Mon.
+      Copyright © {new Date().getFullYear()} Mon. All Rights Reserved.
     </div>
   );
 };
